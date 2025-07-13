7f84afa58bdd3c31a069cd3d8570869a23e5e42c
Manuel Revilla <manuelrevillarj@gmail.com>
2025-07-13 18:55:47 -0500

    cambiando a app.js

--- src/app.js
+++ src/app.js
@@ -12,12 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://127.0.0.1:5173"], // frontend
   })
-)
+);
 
 // Middleware debug
 app.use(debug());
