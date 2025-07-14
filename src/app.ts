42265a57d365c1441383cd64a898c11136fc36d6
Manuel Revilla <manuelrevillarj@gmail.com>
2025-07-13 19:10:04 -0500

    permitir frontend

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"], // frontend
+    origin: [
+      "http://localhost:5173",
+      "https://manuelrevillarj.github.io/PW_G2_F/",
+    ],
   })
 );
 
