c9dbed0451ab74a29360a091c858fe2fc4cd2492
Manuel Revilla <manuelrevillarj@gmail.com>
2025-07-13 20:03:13 -0500

    C:/Soft/Git/

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,38 @@
-import express from "express"
-import cors from "cors"
-import gamesRouter from "./routes/games"
-import sessionsRouter from "./routes/sessions"
-import usersRouter from "./routes/users"
-import debug from "./middleware/debug"
-import { PORT } from "./config"
-import cartRouter from "./routes/cart"
+import express from "express";
+import cors from "cors";
+import gamesRouter from "./routes/games";
+import sessionsRouter from "./routes/sessions";
+import usersRouter from "./routes/users";
+import debug from "./middleware/debug";
+import { PORT } from "./config";
+import cartRouter from "./routes/cart";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://manuelrevillarj.github.io/PW_G2_F/"],
+    origin: [
+      "http://localhost:5173",
+      "https://manuelrevillarj.github.io/PW_G2_F",
+    ],
   })
-)
+);
 
 // Middleware debug
-app.use(debug())
+app.use(debug());
 
 // Rutas
 app.get("/", (_, res) => {
-  res.json("Home Endpoint")
-})
-app.use("/sessions", sessionsRouter)
-app.use("/games", gamesRouter)
-app.use("/users", usersRouter)
-app.use("/cart", cartRouter)
+  res.json("Home Endpoint");
+});
+app.use("/sessions", sessionsRouter);
+app.use("/games", gamesRouter);
+app.use("/users", usersRouter);
+app.use("/cart", cartRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`)
-})
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
